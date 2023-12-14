dbd30eb50be7feddb7e2573ba9da5ba2571595aa
majdalkilany <majdkilany92@gmail.com>
2023-12-14 12:19:16 +0300

    Add User Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PokemonsModule } from './pokemons/pokemons.module';
 import { Pokemon } from './pokemons/pokemons.entity';
+import { UsersModule } from './users/users.module';
+import { User } from './users/Users.entity';
 @Module({
   imports: [
     PokemonsModule,
+    UsersModule,
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [Pokemon],
+      entities: [Pokemon, User],
       synchronize: true,
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
